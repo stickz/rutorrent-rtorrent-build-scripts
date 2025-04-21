0a41bd5d6f869e06bfa09ff0e48665d6b854db14
stickz <stickman002@mail.com>
2025-04-21 13:46:34 -0400

    Fix debug script for rakshasa

--- debug-rtorrent-rakshasa.sh
+++ debug-rtorrent-rakshasa.sh
@@ -9,7 +9,7 @@ git clone https://github.com/rakshasa/libtorrent
 cd ~/build/libtorrent
 autoreconf -vfi
 ./configure --prefix=/usr --enable-aligned
-make -j$(nproc) CXXFLAGS="-O3 -flto=\"$(nproc)\" -Werror=odr -Werror=lto-type-mismatch -Werror=strict-aliasing"
+make -j$(nproc) CXXFLAGS="-g"
 make install
 
 # Build and install rtorrent
@@ -18,7 +18,7 @@ git clone https://github.com/rakshasa/rtorrent
 cd ~/build/rtorrent
 autoreconf -vfi
 ./configure --prefix=/usr --with-xmlrpc-tinyxml2
-make -j$(nproc) CXXFLAGS="-O3 -flto=\"$(nproc)\" -Werror=odr -Werror=lto-type-mismatch -Werror=strict-aliasing"
+make -j$(nproc) CXXFLAGS="-g"
 make install
 
 # Remove build directory
