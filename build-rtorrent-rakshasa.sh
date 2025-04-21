fac36dfba0a41152c1eec462f54e0abf331d4a45
stickz <stickman002@mail.com>
2025-04-20 20:48:28 -0400

    pin to rtorrent 0.15.1

--- build-rtorrent-rakshasa.sh
+++ build-rtorrent-rakshasa.sh
@@ -5,7 +5,7 @@ mkdir ~/build
 
 # Build and install libtorrent
 cd ~/build
-git clone https://github.com/rakshasa/libtorrent
+git clone -b v0.15.1 https://github.com/rakshasa/libtorrent
 cd ~/build/libtorrent
 autoreconf -vfi
 ./configure --prefix=/usr --enable-aligned
@@ -14,7 +14,7 @@ make install
 
 # Build and install rtorrent
 cd ~/build
-git clone https://github.com/rakshasa/rtorrent
+git clone -b v0.15.1 https://github.com/rakshasa/rtorrent
 cd ~/build/rtorrent
 autoreconf -vfi
 ./configure --prefix=/usr --with-xmlrpc-tinyxml2
